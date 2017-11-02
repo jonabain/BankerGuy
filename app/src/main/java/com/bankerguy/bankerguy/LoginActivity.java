c585480e935fc9921ce2144f3e93d5d738edba72
Karan Talreja <ktalreja@umich.edu>
2017-11-02 16:00:07 -0400

    Fixed Jon Bain's STUPID mistake of making the Login Activity backwards compatible

--- app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
+++ app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
@@ -1,15 +1,13 @@
 package com.bankerguy.bankerguy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class LoginActivity extends AppCompatActivity implements Button.OnClickListener {
+public class LoginActivity extends Activity implements Button.OnClickListener {
 
     private Button loginButton;
     private EditText userEmail;
@@ -21,20 +19,18 @@ public class LoginActivity extends AppCompatActivity implements Button.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton = (Button) findViewById(R.id.LoginButton);
-        userEmail = (EditText) findViewById(R.id.UserEmail);
-        userPassword = (EditText) findViewById(R.id.UserPassword);
-        loginTextView = (TextView) findViewById(R.id.LoginTextView);
+        loginButton = findViewById(R.id.LoginButton);
+        userEmail = findViewById(R.id.UserEmail);
+        userPassword = findViewById(R.id.UserPassword);
+        loginTextView = findViewById(R.id.LoginTextView);
 
         loginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view.equals(loginButton)){
-            loginTextView.setText("yeeee");
-        }
+
+
     }
 
-    //test changes
 }
---
+++ app/src/main/java/com/bankerguy/bankerguy/LoginActivity2.java
@@ -0,0 +1,13 @@
+package com.bankerguy.bankerguy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
