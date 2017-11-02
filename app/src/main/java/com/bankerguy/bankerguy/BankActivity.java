647e24742cb58bf3334b21a0c27925c5babfde10
Jonathan Bain <jonabain@umich.edu>
2017-11-02 13:59:23 -0400

    Activities added... by project leader

---
+++ app/src/main/java/com/bankerguy/bankerguy/BankActivity.java
@@ -0,0 +1,13 @@
+package com.bankerguy.bankerguy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BankActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank);
+    }
+}
