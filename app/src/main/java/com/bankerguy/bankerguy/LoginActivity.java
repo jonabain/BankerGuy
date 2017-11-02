c019103ff40ffbe072a2054448dbbd5b360dd94e
Jonathan Bain <jonabain@umich.edu>
2017-11-02 14:31:30 -0400

    Pushing at the end of class

--- app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
+++ app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
@@ -2,12 +2,37 @@ package com.bankerguy.bankerguy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity implements Button.OnClickListener {
+
+    private Button loginButton;
+    private EditText userEmail;
+    private EditText userPassword;
+    private TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.LoginButton);
+        userEmail = (EditText) findViewById(R.id.UserEmail);
+        userPassword = (EditText) findViewById(R.id.UserPassword);
+        loginTextView = (TextView) findViewById(R.id.LoginTextView);
+
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.equals(loginButton)){
+            loginTextView.setText("yeeee");
+        }
     }
 }
