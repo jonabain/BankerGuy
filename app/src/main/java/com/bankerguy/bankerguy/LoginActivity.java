0a2189f6dac7fd0b6b0833335252e05523148b36
SigmanZero <sigmanzero@gmail.com>
2017-11-07 13:12:06 -0500

    Implemented Login These changes are untested

--- app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
+++ app/src/main/java/com/bankerguy/bankerguy/LoginActivity.java
@@ -1,36 +1,102 @@
 package com.bankerguy.bankerguy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends Activity implements Button.OnClickListener {
 
     private Button loginButton;
+    private Button createButton;
     private EditText userEmail;
     private EditText userPassword;
-    private TextView loginTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         loginButton = findViewById(R.id.LoginButton);
+        createButton = findViewById(R.id.CreateButton);
         userEmail = findViewById(R.id.UserEmail);
         userPassword = findViewById(R.id.UserPassword);
-        loginTextView = findViewById(R.id.LoginTextView);
 
         loginButton.setOnClickListener(this);
+        createButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            goToHome();
+        }
     }
 
     @Override
     public void onClick(View view) {
+        if(view == loginButton){
+            login(userEmail.getText().toString(), userPassword.getText().toString());
+        } else if (view == createButton){
+            create(userEmail.getText().toString(), userPassword.getText().toString());
+        }
+    }
 
+    private void login(final String email, final String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            goToHome();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login unsuccessful.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
+    private void create(final String email, final String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            login(email, password);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Account creation failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void goToHome(){
+        Intent home = new Intent(this, HomeActivity.class);
+        this.startActivity(home);
     }
 
 }
